5100cf5b6e6d006e0f1fe093ae788bcdcdd62218
Chhagan Lal Mathuriya <chhagan@Chhagans-MacBook-Pro-2.local>
2017-07-15 17:10:22 +0530

    changes for extent reporter

--- src/main/java/com/newton/tests/BaseTest.java
+++ src/main/java/com/newton/tests/BaseTest.java
@@ -44,7 +44,7 @@ public class BaseTest {
 
 		// reporter = MyReporter.getInstance();
 		System.out.println("existing value : " + System.getProperty("hudson.model.DirectoryBrowserSupport.CSP"));
-		System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "");
+		System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "sandbox allow-scripts;");
 
 		System.out.println("after value : " + System.getProperty("hudson.model.DirectoryBrowserSupport.CSP"));
 		util = Util.getInstance();
